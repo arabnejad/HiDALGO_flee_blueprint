9b93aea9d1a20f2398f5a3d382fc346483783373
Hamid <hamid.arabnejad@gmail.com>
2021-09-10 15:42:30 +0100

    Adds muscle3 installation

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -62,7 +62,7 @@ if [[ "${MSCALE^^}" == "FALSE" ]]; then
     fi
 
 else
-
+    pip3 install --user -U muscle3
     if [[ "${MSCALE_COUPLING_TYPE^^}" == "FILE" ]]; then
         echo "Run multi-scale simulation . . ." | tee -a "$LOG_RUN_JOB"
         bash run_file_coupling.sh --NUM_WORKERS $MSCALE_NUM_WORKERS --cores $MSCALE_CORES_PER_WORKER --INPUT_DATA_DIR $MSCALE_INPUT_DATA_DIR --LOG_EXCHANGE_DATA $MSCALE_LOG_EXCHANGE_DATA --WEATHER_COUPLING $MSCALE_WEATHER_COUPLING
